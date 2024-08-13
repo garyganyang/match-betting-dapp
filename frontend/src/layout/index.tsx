d00acfb80ab68545a0cad72a891a3d25903efdfe
ganyang <ganyang@cjiedata.com>
2024-08-14 00:33:53 +0800

    UI pages

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,10 +1,12 @@
 import "./index.scss";
 // import {UploadOutlined, UserOutlined, VideoCameraOutlined} from '@ant-design/icons';
 import store from "/src/redux/reduxStore";
-import {Layout, theme} from 'antd';
+import {Layout, theme, ConfigProvider, Button} from 'antd';
 import {Outlet} from "react-router-dom";
-import {FunctionComponent, useState} from "react";
+import {FunctionComponent, useState, useContext} from "react";
 import Aside from "./aside"
+import {css} from '@emotion/css';
+import {GithubFilled} from "@ant-design/icons";
 
 // 使用 React.FunctionComponent 定义 函数组件 的类型
 // 使用 React.Component         定义   类组件 的类型
@@ -19,30 +21,98 @@ const MyLayout: FunctionComponent = () => {
 
     const {token: {colorBgContainer, borderRadiusLG}} = theme.useToken();
 
+    const {getPrefixCls} = useContext(ConfigProvider.ConfigContext);
+    const rootPrefixCls = getPrefixCls();
+    const linearGradientButton = css`
+        &.${rootPrefixCls}-btn-primary:not([disabled]):not(.${rootPrefixCls}-btn-dangerous) {
+            border-width: 0;
+
+            > span {
+                position: relative;
+            }
+
+            &::before {
+                content: '';
+                background: linear-gradient(135deg, #6253E1, #04BEFE);
+                position: absolute;
+                inset: 0;
+                opacity: 1;
+                transition: all 0.3s;
+                border-radius: inherit;
+            }
+
+            &:hover::before {
+                opacity: 0;
+            }
+        }
+    `;
+    const connectWallet = () => {
+
+    }
+
 
     return ( //多行箭头函数返回 使用()
-        <Layout>
-            <Sider breakpoint="lg"
-                   collapsedWidth="0"
-                   onBreakpoint={(broken) => {
-                       console.log(broken);
-                   }}
-                   onCollapse={(collapsed, type) => {
-                       console.log(collapsed, type);
-                   }}>
-                <div className="demo-logo-vertical"/>
-                <Aside/>
-            </Sider>
+        <ConfigProvider
+            theme={{
+                components: {
+                    Layout: {
+                        siderBg: '#24272a',
+                        bodyBg: "#141618",
+                        footerBg: '#141618',
+                        footerPadding: "6px 50px"
+                    },
+                    Menu: {
+                        darkItemBg: '#24272a',
+                        darkItemColor: '#FFF',
+                        darkItemSelectedBg: '#ffffff0a',
+                        darkItemHoverBg: '#ffffff0a'
+                    },
+
+                },
+            }}
+        >
             <Layout>
-                <Header style={{padding: '0 16px', background: colorBgContainer}}>Header subscribe Redux state on "name" changes - {name}</Header>
-                <Content style={{margin: '16px 16px 0', padding: 16, height: 'calc(100vh - 64px - 69px - 16px)', borderRadius: '8px', background: colorBgContainer}}>
-                    <Outlet/>
-                </Content>
-                <Footer style={{textAlign: 'center'}}>
-                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
-                </Footer>
+                <Sider breakpoint="lg"
+                       collapsedWidth="0"
+                       onBreakpoint={(broken) => {
+                           console.log(broken);
+                       }}
+                       onCollapse={(collapsed, type) => {
+                           console.log(collapsed, type);
+                       }}
+                       style={{borderRight: "1px solid #848c9629"}}
+                >
+                    <div className="demo-logo-vertical">
+                        <div className="" style={{fontSize: 17, fontWeight: "bold"}}>Match Betting</div>
+                        <div style={{}}>an Ethereum DApp</div>
+                    </div>
+                    <Aside/>
+                </Sider>
+                <Layout>
+                    {/*<Header style={{padding: '0 16px', background: colorBgContainer}}>Header subscribe Redux state on "name" changes - {name}</Header>*/}
+                    <Header className="text-white flex" style={{padding: '0 16px', background: "#141618", borderBottom: "1px solid #848c9629"}}>
+                        <div className="flex-1"></div>
+                        <div className="flex-1 text-white flex items-center text-right justify-end">
+                            <ConfigProvider
+                                button={{
+                                    className: linearGradientButton,
+                                }}
+                            >
+                                <a className="text-2xl mr-4 " href="https://github.com/garyganyang" target="_blank" rel="noopener noreferrer"><GithubFilled/></a>
+                                <Button type="primary" onClick={connectWallet}>Connect wallet</Button>
+                            </ConfigProvider>
+                        </div>
+
+                    </Header>
+                    <Content className="text-white overflow-y-scroll" style={{margin: '0', padding: 16, height: 'calc(100vh - 64px - 36px)', borderRadius: '0px',}}>
+                        <Outlet/>
+                    </Content>
+                    <Footer className="text-white" style={{textAlign: 'center', borderTop: "1px solid #848c9629"}}>
+                        Match Betting ©{new Date().getFullYear()} Created garyganyang
+                    </Footer>
+                </Layout>
             </Layout>
-        </Layout>
+        </ConfigProvider>
     );
 }
 
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -14,51 +14,52 @@ export const routerItem: Array<object> = [
     {
         path: "/",
         key: "/",
-        label: "首页",
+        label: "竞彩足球",
         hidden: true, // 菜单隐藏
         element: <Navigate to="/layout/home"/>
     },
-    {
-        path: "/login",
-        key: "login",
-        label: "登录",
-        element: <LoginComponent/>,
-        meta: {
-            noAuth: true    //不需要token检验
-        }
-    },
+    // {
+    //     path: "/login",
+    //     key: "login",
+    //     label: "登录",
+    //     element: <LoginComponent/>,
+    //     meta: {
+    //         noAuth: true    //不需要token检验
+    //     }
+    // },
     {
         path: "/layout",
         key: "layout",
         label: "控制台",
+        hidden: true, // 菜单隐藏
         element: <LayoutComponent/>,
         children: [
             {
                 path: "home",
                 key: "home",
-                label: "首页",
+                label: "竞彩足球",
                 element: lazyLoad(lazy(() => import("/src/pages/home/index"))),//这里是延迟加载
             },
-            {
-                path: "user",
-                key: "user",
-                label: "用户",
-                element: <User/>,
-                children: [
-                    {
-                        path: "home1",
-                        key: "home1",
-                        label: "首页1",
-                        element: <HomeComponent/>
-                    },
-                    {
-                        path: "user1",
-                        key: "user1",
-                        label: "用户1",
-                        element: <User/>
-                    },
-                ],
-            },
+            // {
+            //     path: "user",
+            //     key: "user",
+            //     label: "用户",
+            //     element: <User/>,
+            //     children: [
+            //         {
+            //             path: "home1",
+            //             key: "home1",
+            //             label: "首页1",
+            //             element: <HomeComponent/>
+            //         },
+            //         {
+            //             path: "user1",
+            //             key: "user1",
+            //             label: "用户1",
+            //             element: <User/>
+            //         },
+            //     ],
+            // },
         ],
     },
     {
